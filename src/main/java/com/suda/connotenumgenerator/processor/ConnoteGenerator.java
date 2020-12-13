4d634a259ad511248434aaf855c741a55db4df13
Sudarsan <sutharshan.g@gmail.com>
2020-12-13 15:51:23 +1100

    New test case for edge cases, design process, runtime added.

--- src/main/java/com/suda/connotenumgenerator/processor/ConnoteGenerator.java
+++ src/main/java/com/suda/connotenumgenerator/processor/ConnoteGenerator.java
@@ -2,6 +2,9 @@ package com.suda.connotenumgenerator.processor;
 
 import com.suda.connotenumgenerator.domain.CarrierAccount;
 
+/**
+ * This class is used to generate Consignment Note Number for Freights using the CarrierAccount POJO
+ */
 public class ConnoteGenerator {
     static final int EVEN_SERIES_MULTI_FACTOR = 3;
     static final int ODD_SERIES_MULTI_FACTOR = 7;
@@ -10,6 +13,14 @@ public class ConnoteGenerator {
     static final String NULL_CONS_INDEX = "Provided Consignment Index is Null";
     static final String NEGATIVE_START_POS = "Provided Start position cannot be negative";
 
+    /**
+     * This method generates the next Connote Number by merging the courier Prefix, Account Number, generated consignment Index & checksum.
+     * This method validates if the new index is within the provided range.
+     * Since this method is dependant only on this POJO, it's possible to extend this to be used as a REST micro service/ any other required implementation.
+     * Checksum is calculated in the optimum way to find the gap with next multiplication of 10.
+     * @param carrierAccount The POJO which provides the prefix, Account Number, index digits, last index, range start & end values.
+     * @return The next Consignment Note Number (Connote Number) generated.
+     */
     public String generate(CarrierAccount carrierAccount) {
         if(carrierAccount.getLastUsedIndex() < 0 || carrierAccount.getRangeStart() < 0 || carrierAccount.getRangeEnd() <= 0) {
             System.out.println("calculateConsignmentIndex: Index and Digits cannot be <= 0");
@@ -34,7 +45,7 @@ public class ConnoteGenerator {
         int totalSum = (calculateSum(consignmentIndex,0)) * EVEN_SERIES_MULTI_FACTOR
                 + (calculateSum(consignmentIndex, 1)) * ODD_SERIES_MULTI_FACTOR;
 
-        final int checksum = CHECKSUM_MULTI_FACTOR - (totalSum % CHECKSUM_MULTI_FACTOR);        //TODO how this works
+        final int checksum = (totalSum % CHECKSUM_MULTI_FACTOR ==0) ? 0: (CHECKSUM_MULTI_FACTOR - (totalSum % CHECKSUM_MULTI_FACTOR));   //This will give the gap between the sum and next multiple of 10
 
         return carrierAccount.getPrefix() +
                 carrierAccount.getAccountNumber() +
@@ -42,6 +53,15 @@ public class ConnoteGenerator {
                 checksum;
     }
 
+    /**
+     * This is a helper method to generate the consignment Index using the provided index.
+     * This method adds required 0s to the prefix to generate index with length of the digits always.
+     * Eg: An index with 19456 and digits 10 will produce 0000019456, where there are five 0s added to front to make it 10 digits.
+     * Note: This method is marked for package access to make sure it can be unit tested.
+     * @param index The index which needs to be padded with 0s.
+     * @param digits The length of the consignment index which is expected to be produced.
+     * @return The consignment Index with the size of provided digit value.
+     */
     String calculateConsignmentIndex(final int index, final int digits) {
         if(digits <= 0 || index <= 0) {
             System.out.println("calculateConsignmentIndex: Index and Digits cannot be <= 0");
@@ -50,7 +70,16 @@ public class ConnoteGenerator {
         return String.format("%0"+ digits + "d", index);
     }
 
-    int calculateSum(String consignmentIndex, int startingPosition) {
+    /**
+     * This is a helper method to calculate the sum of every second number in the provided consignment Index.
+     * The starting position is configurable which helps to reuse this for both even & odd position sum calculation.
+     * Note: This method is marked for package access to make sure it can be unit tested.
+     * @param consignmentIndex The index which needs to be calculated with sum to help the checksum generation.
+     * @param startingPosition The starting position from right. Pass this as 0 to find the even position elements' total.
+     *                         Pass this as 1 to find the odd position elements' total.
+     * @return The sum of the every second digit in the Index.
+     */
+    int calculateSum(final String consignmentIndex, final int startingPosition) {
         assert consignmentIndex != null : NULL_CONS_INDEX;
         assert startingPosition >= 0 : NEGATIVE_START_POS;
 
--- src/test/java/com/suda/connotenumgenerator/processor/ConnoteGeneratorTest.java
+++ src/test/java/com/suda/connotenumgenerator/processor/ConnoteGeneratorTest.java
@@ -30,7 +30,7 @@ public class ConnoteGeneratorTest {
 
     @Test
     public void testCalculateConsignmentIndexForLargeValueThanDigits() {
-        assertEquals(connoteGenerator.calculateConsignmentIndex(2034334, 6), "2034334");
+        assertEquals("2034334", connoteGenerator.calculateConsignmentIndex(2034334, 6));
     }
 
     @Test
@@ -58,22 +58,22 @@ public class ConnoteGeneratorTest {
         exception = assertThrows(AssertionError.class, () -> connoteGenerator.calculateSum("0000019345", Integer.MIN_VALUE));
         assertEquals(NEGATIVE_START_POS, exception.getMessage());
 
-        assertEquals(connoteGenerator.calculateSum("45", 0), 0);
-        assertEquals(connoteGenerator.calculateSum("0000019345", 10), 0);
-        assertEquals(connoteGenerator.calculateSum("0000019345", 12), 0);
-        assertEquals(connoteGenerator.calculateSum("0000019345", Integer.MAX_VALUE), 0);
+        assertEquals(0, connoteGenerator.calculateSum("45", 0));
+        assertEquals(0, connoteGenerator.calculateSum("0000019345", 10));
+        assertEquals(0, connoteGenerator.calculateSum("0000019345", 12));
+        assertEquals(0, connoteGenerator.calculateSum("0000019345", Integer.MAX_VALUE));
     }
 
     @Test
     public void testCalculateSum() {
-        assertEquals(connoteGenerator.calculateSum("0000019605", 0), 12);
-        assertEquals(connoteGenerator.calculateSum("0000019605", 1), 9);
+        assertEquals(12, connoteGenerator.calculateSum("0000019605", 0));
+        assertEquals(9, connoteGenerator.calculateSum("0000019605", 1));
 
-        assertEquals(connoteGenerator.calculateSum("00019708", 0), 16);
-        assertEquals(connoteGenerator.calculateSum("00019708", 1), 9);
+        assertEquals(16, connoteGenerator.calculateSum("00019708", 0));
+        assertEquals(9, connoteGenerator.calculateSum("00019708", 1));
 
-        assertEquals(connoteGenerator.calculateSum("0019654", 0), 11);
-        assertEquals(connoteGenerator.calculateSum("0019654", 1), 14);
+        assertEquals(11, connoteGenerator.calculateSum("0019654", 0));
+        assertEquals(14, connoteGenerator.calculateSum("0019654", 1));
     }
 
     @Test
@@ -90,15 +90,16 @@ public class ConnoteGeneratorTest {
 
     @Test
     public void testGenerateForSuccessfulOperationForDiffDigits() {
-        assertEquals(connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 10, 19604, 19000, 20000)), "FMCC123ABC00000196051");
-        assertEquals(connoteGenerator.generate(new CarrierAccount("ABCD", "XY12345", 8, 19604, 19000, 20000)), "ABCDXY12345000196051");
-        assertEquals(connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 12, 19604, 19000, 20000)), "FMCC123ABC0000000196051");
+        assertEquals("FMCC123ABC00000196051", connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 10, 19604, 19000, 20000)));
+        assertEquals("ABCDXY12345000196051", connoteGenerator.generate(new CarrierAccount("ABCD", "XY12345", 8, 19604, 19000, 20000)));
+        assertEquals("FMCC123ABC0000000196051", connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 12, 19604, 19000, 20000)));
     }
 
     @Test
     public void testGenerateForDifferentChecksum() {
-        assertEquals(connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 10, 19554, 19000, 20000)), "FMCC123ABC00000195559");
-        assertEquals(connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 10, 32455, 30000, 40000)), "FMCC123ABC00000324562");
+        assertEquals("FMCC123ABC00000195559", connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 10, 19554, 19000, 20000)));
+        assertEquals("FMCC123ABC00000324562", connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 10, 32455, 30000, 40000)));
+        assertEquals("FMCC123ABC00000354530", connoteGenerator.generate(new CarrierAccount("FMCC", "123ABC", 10, 35452, 30000, 40000)));
 
     }
 }
